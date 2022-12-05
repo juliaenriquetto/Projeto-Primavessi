0d190857b2f98cafd140aee03f9a30ec92d416b0
Júlia Enriquetto de Brito <107726291+juliaenriquetto@users.noreply.github.com>
2022-12-05 12:28:51 -0300

    Configurações atualizadas

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.set("view engine","ejs") 
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.listen(8080, () => {
-    console.log('API funcionando');
+    console.log('API funcionando!');
 })
